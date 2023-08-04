998b34f0fd2c03de10f1d806a094d3e6a354e3cb
Michael3573 <126564353+Michael3573@users.noreply.github.com>
2023-08-04 19:47:24 +0200

    Apdate1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector(".gallery");
+const createMarkup = (array) => {
+  const markup = array
+    .map((image) => {
+      return `<li class="gallery__item">
+<a class="gallery__link" 
+href="${image.original}">
+<img class="gallery__image"
+src="${image.preview}"
+data-source="${image.original}"
+alt="${image.description}"/></a></li>`;
+    })
+    .join("");
+
+  galleryListEl.innerHTML = markup;
+};
+
+createMarkup(galleryItems);
+
+const galeryClick = (event) => {
+  event.preventDefault();
+  if (event.target.tagName !== "IMG") {
+    return;
+  }
+  const escClose = (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  };
+
+  const imagesSrc = event.target.getAttribute("data-source");
+  const instance = basicLightbox.create(
+    `
+  <div><img src="${imagesSrc}" width = "1000" height="700"></div>`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", escClose);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", escClose);
+      },
+    }
+  );
+
+  instance.show();
+};
+
+galleryListEl.addEventListener("click", galeryClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector(".gallery");
+const createMarkup = (array) => {
+  const markup = array
+    .map((image) => {
+      return `<li class="gallery__item">
+<a class="gallery__link" 
+href="${image.original}">
+<img class="gallery__image"
+src="${image.preview}"
+alt="${image.description}"/></a></li>`;
+    })
+    .join("");
+
+  galleryListEl.innerHTML = markup;
+};
+
+createMarkup(galleryItems);
+
+var lightbox = new SimpleLightbox(".gallery__link", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
